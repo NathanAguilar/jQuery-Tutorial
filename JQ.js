b3895a7d712fb950a95711bc84a8de08c268c28c
Nathan  Aguilar <nathanaguilar@Nathans-MBP.lan>
2023-12-06 15:12:55 -0800

    jQuery Selectors Intro

--- JQ.js
+++ JQ.js
@@ -1,4 +1,9 @@
 function func1()
 {
-    $("#img1").fadeToggle(3000);
+    //$(element name).action()
+    //document.getElementByTagName(element name)
+
+    //$("div").css('background-color','orange');
+    //$("#p1").css('font-style','italic');
+    $(".para").css('font-style','italic')
 }
